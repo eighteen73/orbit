b36c4357826395c77f94c7357611a648bb5f2c6d
Ed Jeavons <ed@orphans.co.uk>
2022-04-22 12:18:03 +0100

    Initial commit

---
+++ orbit.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Orbit
+ * Plugin URI:      https://code.orphans.co.uk/packages/wordpress/orbit
+ * Description:     Opinionated WordPress behaviour overrides
+ * Author:          Orphans Press
+ * Author URI:      https://orphans.co.uk/websites
+ * Text Domain:     orbit
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Orbit
+ */
+
+// Your code starts here.
