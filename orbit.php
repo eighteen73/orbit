4dd5049c0fcaf9331448253979448f7cf46e6df9
Ed Jeavons <ed@orphans.co.uk>
2022-04-22 17:55:40 +0100

    Class loading

--- orbit.php
+++ orbit.php
@@ -3,7 +3,7 @@
  * Plugin Name:     Orbit
  * Plugin URI:      https://code.orphans.co.uk/packages/wordpress/orbit
  * Description:     Opinionated WordPress behaviour overrides
- * Author:          Orphans Press
+ * Author:          Orphans Web Team
  * Author URI:      https://orphans.co.uk/websites
  * Text Domain:     orbit
  * Domain Path:     /languages
@@ -12,4 +12,22 @@
  * @package         Orbit
  */
 
-// Your code starts here.
+namespace Orbit;
+
+spl_autoload_register(
+	function( $class_name ) {
+		$path_parts = explode( '\\', $class_name );
+
+		if ( ! empty( $path_parts ) ) {
+			$package = $path_parts[0];
+
+			unset( $path_parts[0] );
+
+			if ( 'Orbit' === $package ) {
+				require_once __DIR__ . '/src/classes/' . implode( '/', $path_parts ) . '.php';
+			}
+		}
+	}
+);
+
+Admin\Settings::instance()->setup();
