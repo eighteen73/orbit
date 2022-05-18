803d76b76580a625fdb755c197c349d01e656a06
Ed Jeavons <ed@orphans.co.uk>
2022-05-18 15:21:09 +0100

    Add missing rewrite_rules_array filter callback

--- includes/classes/Security/RemoveHeadLinks.php
+++ includes/classes/Security/RemoveHeadLinks.php
@@ -30,6 +30,21 @@ class RemoveHeadLinks extends Singleton {
 		remove_action( 'rest_api_init', 'wp_oembed_register_route' );
 		remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result' );
 		add_filter( 'embed_oembed_discover', '__return_false' );
-		add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
+		add_filter( 'rewrite_rules_array', [ $this, 'disable_embeds_rewrites' ] );
+	}
+
+	/**
+	 * Remove embed redirects
+	 *
+	 * @param array $rules Redirect rules
+	 * @return array
+	 */
+	public function disable_embeds_rewrites( array $rules ): array {
+		foreach ( $rules as $rule => $rewrite ) {
+			if ( false !== strpos( $rewrite, 'embed=true' ) ) {
+				unset( $rules[ $rule ] );
+			}
+		}
+		return $rules;
 	}
 }
