558fbc5ee14c4202b180dce00c69d7970bff41dc
Dan Hudson <dan@orphans.co.uk>
2025-08-04 12:36:40 +0100

    New: only apply template to non html emails, specific gf scenario

--- includes/classes/BrandedEmails.php
+++ includes/classes/BrandedEmails.php
@@ -27,6 +27,7 @@ class BrandedEmails {
 		}
 
 		add_filter( 'wp_mail', [ $this, 'apply_branded_email_template' ] );
+		add_filter( 'gform_html_message_template_pre_send_email', [ $this, 'apply_branded_email_template_to_gf_notifications' ] );
 	}
 
 	/**
@@ -36,19 +37,71 @@ class BrandedEmails {
 	 * @return array Modified $args array with the email message wrapped in branded HTML.
 	 */
 	public function apply_branded_email_template( $args ) {
+		$headers = [];
+
+		// Convert headers to an array if they aren't already
+		if ( empty( $args['headers'] ) ) {
+			$headers = [];
+		} elseif ( is_string( $args['headers'] ) ) {
+			$headers = explode( "\n", str_replace( "\r\n", "\n", $args['headers'] ) );
+		} elseif ( is_array( $args['headers'] ) ) {
+			$headers = $args['headers'];
+		}
+
+		// Check if Content-Type is already set to HTML
+		$content_type_is_html = false;
+		foreach ( $headers as $header ) {
+			if ( stripos( $header, 'Content-Type:' ) !== false && stripos( $header, 'text/html' ) !== false ) {
+				$content_type_is_html = true;
+				break;
+			}
+		}
+
+		// If email is already HTML return original $args
+		if ( $content_type_is_html ) {
+			return $args;
+		}
+
 		ob_start();
 
 		Templates::include_template_part(
 			'branded-emails/email-template.php',
 			[
 				'email_content' => $args['message'],
+				'email_subject' => isset( $args['subject'] ) ? $args['subject'] : '',
 			]
 		);
 
 		$styled_message = ob_get_clean();
 
+		// Remove existing Content-Type headers (at this point we've already established it's not HTML)
+		$headers = array_filter( $headers, function ( $header ) {
+			return stripos( $header, 'Content-Type:' ) === false;
+		} );
+
+		// Add HTML Content-Type
+		$headers[] = 'Content-Type: text/html; charset=UTF-8';
+
+		// Set updated headers and message
+		$args['headers'] = $headers;
 		$args['message'] = $styled_message;
 
 		return $args;
+	}
+
+	public function apply_branded_email_template_to_gf_notifications( $template ) {
+		ob_start();
+
+		Templates::include_template_part(
+			'branded-emails/email-template.php',
+			[
+				'email_content' => '{message}',
+				'email_subject' => '{subject}',
+			]
+		);
+
+		$template = ob_get_clean();
+
+		return $template;
 	}
 }
--- template-parts/branded-emails/email-template.php
+++ template-parts/branded-emails/email-template.php
@@ -16,17 +16,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-  <meta charset="UTF-8">
-  <?php Templates::include_template_part( 'branded-emails/email-styles.php' ); ?>
-</head>
-<body>
-  <div class="email-wrapper">
-    <?php echo wpautop( $email_content ); ?>
+	<head>
+		<meta charset="UTF-8">
+		<title><?php echo esc_attr( $email_subject ); ?></title>
+		<?php Templates::include_template_part( 'branded-emails/email-styles.php' ); ?>
+	</head>
+	<body>
+		<div class="email-wrapper">
+			<?php echo wpautop( $email_content ); ?>
 
-    <div class="email-footer">
-      Sent from <?php echo esc_html( get_bloginfo('name') ); ?>
-    </div>
-  </div>
-</body>
+			<div class="email-footer">
+				Sent from <?php echo esc_html( get_bloginfo('name') ); ?>
+			</div>
+		</div>
+	</body>
 </html>
