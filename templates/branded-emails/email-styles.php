c658d3fdbdc371049ec8ede74b5e40f50bd64f65
Dan Hudson <dan@orphans.co.uk>
2025-08-05 11:46:36 +0100

    New: bring template inline with WooCommerce base

--- templates/branded-emails/email-styles.php
+++ templates/branded-emails/email-styles.php
@@ -10,45 +10,218 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-$background_color = apply_filters( 'orbit_branded_emails_background_color', '#f4f4f4' );
+$global_settings = wp_get_global_settings();
+$background_color = apply_filters( 'orbit_branded_emails_background_color', '#ffffff' );
 $body_background_color = apply_filters( 'orbit_branded_emails_body_background_color', '#ffffff' );
-$body_border_color = apply_filters( 'orbit_branded_emails_body_border_color', '#ddd' );
-$body_max_width = apply_filters( 'orbit_branded_emails_body_max_width', '800px' );
-$body_text_color = apply_filters( 'orbit_branded_emails_body_text_color', '#000' );
-$footer_text_color = apply_filters( 'orbit_branded_emails_footer_text_color', '#999' );
+$body_border_color = apply_filters( 'orbit_branded_emails_body_border_color', $global_settings['custom']['color']['border'] ?? '#EDEFF1' );
+$body_text_color = apply_filters( 'orbit_branded_emails_body_text_color', $global_settings['color']['contrast'] ?? '#3F474D' );
+$link_color = apply_filters( 'orbit_branded_emails_link_color', $global_settings['custom']['color']['link'] ?? '#8549FF' );
+$footer_text_color = apply_filters( 'orbit_branded_emails_footer_text_color', $global_settings['color']['contrast'] ?? '#3F474D' );
+$font_family = '"Helvetica Neue", Helvetica, Roboto, Arial, sans-serif';
 ?>
 
 <style>
 	body {
 		background: <?php echo esc_attr( $background_color ); ?>;
-		font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif;
-		padding: 20px;
-	}
-
-	.email-wrapper {
-		background: <?php echo esc_attr( $body_background_color ); ?>;
-		border: 1px solid <?php echo esc_attr( $body_border_color ); ?>;
-		border-radius: 6px;
-		color: <?php echo esc_attr( $body_text_color ); ?>;
-		max-width: <?php echo esc_attr( $body_max_width ); ?>;
-		margin: 0 auto;
-		padding: 20px;
-	}
-
-	.email-header img {
-		max-height: 100px;
-		width: auto;
-	}
-
-	.email-content {
-		margin-bottom: 40px;
-		margin-top: 40px;
-	}
-
-	.email-footer {
-		color: <?php echo esc_attr( $footer_text_color ); ?>;
-		font-size: 12px;
-		margin-top: 20px;
+		font-family: <?php echo $font_family; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>;
+		padding: 0;
 		text-align: center;
+	}
+
+	#outer_wrapper {
+		background: <?php echo esc_attr( $background_color ); ?>;
+	}
+
+	#inner_wrapper {
+		background-color: <?php echo esc_attr( $body_background_color ); ?>;
+		border-radius: 6px;
+	}
+
+	#email_wrapper {
+		margin: 0 auto;
+		padding: 24px 0;
+		-webkit-text-size-adjust: none !important;
+		width: 100%;
+		max-width: 600px;
+	}
+
+	#template_container {
+		background-color: <?php echo esc_attr( $body_background_color ); ?>;
+		border-radius: 3px !important;
+	}
+
+	#template_header {
+		background-color: <?php echo esc_attr( $body_background_color ); ?>;
+		border-radius: 3px 3px 0 0 !important;
+		color: <?php echo esc_attr( $body_text_color ); ?>;
+		border-bottom: 0;
+		font-weight: bold;
+		line-height: 100%;
+		vertical-align: middle;
+		font-family: <?php echo $font_family; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>;
+	}
+
+	#template_header h1 {
+		color: <?php echo esc_attr( $body_text_color ); ?>;
+		background-color: inherit;
+	}
+
+	#template_header_image {
+		padding: 32px 32px 0;
+	}
+
+	#template_header_image p {
+		margin-bottom: 0;
+		text-align: left;
+	}
+
+	#template_header_image img {
+		width: <?php echo esc_attr( $logo_image_width ); ?>px;
+	}
+
+	.email-logo-text {
+		color: <?php echo esc_attr( $link_color ); ?>;
+		font-family: <?php echo $font_family; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>;
+		font-size: 18px;
+	}
+
+	.email-introduction {
+		padding-bottom: 24px;
+	}
+
+	#template_footer td {
+		padding: 0;
+	}
+
+	#template_footer #credit {
+		border: 0;
+		border-top: 1px solid <?php echo esc_attr( $body_border_color ); ?>;
+		color: <?php echo esc_attr( $footer_text_color ); ?>;
+		font-family: <?php echo $font_family; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>;
+		font-size: 12px;
+		line-height: 140%;
+		text-align: center;
+		padding: 32px;
+	}
+
+	#template_footer #credit p {
+		margin: 0;
+	}
+
+	#body_content {
+		background-color: <?php echo esc_attr( $body_background_color ); ?>;
+	}
+
+	#body_content table td th {
+		padding: 12px;
+	}
+	#body_content p {
+		margin: 0 0 16px;
+	}
+
+	#body_content_inner {
+		color: <?php echo esc_attr( $body_text_color ); ?>;
+		font-family: <?php echo $font_family; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>;
+		font-size: 16px;
+		line-height: 150%;
+		text-align: left;
+	}
+
+	#body_content_inner_cell {
+		padding: 20px 32px 32px;
+	}
+
+	.td {
+		color: <?php echo esc_attr( $body_text_color ); ?>;
+		vertical-align: middle;
+	}
+
+	#header_wrapper {
+		padding: 20px 32px 0;
+		display: block;
+	}
+
+	#template_footer #credit,
+	#template_footer #credit a {
+		color: <?php echo esc_attr( $footer_text_color ); ?>;
+	}
+
+	h1 {
+		color: <?php echo esc_attr( $body_text_color ); ?>;
+		font-family: <?php echo $font_family; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>;
+		font-size: 32px;
+		font-weight: 700;
+		letter-spacing: -1px;
+		line-height: 120%;
+		margin: 0;
+		text-align: left;
+	}
+
+	h2 {
+		color: <?php echo esc_attr( $body_text_color ); ?>;
+		display: block;
+		font-family: <?php echo $font_family; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>;
+		font-size: 20px;
+		font-weight: bold;
+		line-height: 160%;
+		margin: 0 0 18px;
+		text-align: left;
+	}
+
+	h3 {
+		color: <?php echo esc_attr( $body_text_color ); ?>;
+		display: block;
+		font-family: <?php echo $font_family; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>;
+		font-size: 16px;
+		font-weight: bold;
+		line-height: 160%;
+		margin: 16px 0 8px;
+		text-align: left;
+	}
+
+	a {
+		color: <?php echo esc_attr( $link_color ); ?>;
+		font-weight: normal;
+		text-decoration: underline;
+	}
+
+	img {
+		border: none;
+		display: inline-block;
+		font-size: 14px;
+		font-weight: bold;
+		height: auto;
+		outline: none;
+		text-decoration: none;
+		text-transform: capitalize;
+		vertical-align: middle;
+		margin-left: 24px;
+		max-width: 100%;
+	}
+
+	/**
+	* Media queries are not supported by all email clients, however they do work on modern mobile
+	* Gmail clients and can help us achieve better consistency there.
+	*/
+	@media screen and (max-width: 600px) {
+		#template_header_image {
+			padding: 16px 10px 0 !important;
+		}
+
+		#header_wrapper {
+			padding: 16px 10px 0 !important;
+		}
+
+		#header_wrapper h1 {
+			font-size: 24px !important;
+		}
+
+		#body_content_inner_cell {
+			padding: 10px !important;
+		}
+
+		#body_content_inner {
+			font-size: 12px !important;
+		}
 	}
 </style>
--- templates/branded-emails/email-template.php
+++ templates/branded-emails/email-template.php
@@ -15,37 +15,105 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 $header_logo = apply_filters( 'orbit_branded_emails_header_logo', '' );
+$site_name = get_bloginfo( 'name', 'display' );
 $email_content = $args['email_content'] ?? '';
 $email_subject = $args['email_subject'] ?? get_bloginfo( 'name', 'display' );
 ?>
 
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 	<head>
-		<meta charset="UTF-8">
+		<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
+		<meta content="width=device-width, initial-scale=1.0" name="viewport">
 		<title><?php echo esc_attr( $email_subject ); ?></title>
 		<?php Templates::include_template( 'branded-emails/email-styles.php' ); ?>
 	</head>
-
-	<body>
-		<div class="email-wrapper">
-			<div class="email-header">
-				<?php
-				if ( ! empty( $header_logo ) ) {
-					echo '<p style="margin-top:0;"><img src="' . esc_url( $header_logo ) . '" alt="' . esc_attr( get_bloginfo( 'name', 'display' ) ) . '" /></p>';
-				} else {
-					echo '<h1>' . esc_html( get_bloginfo( 'name' ) ) . '</h1>';
-				}
-				?>
-			</div>
-
-			<div class="email-content">
-				<?php echo wp_kses_post( wpautop( $email_content ) ); ?>
-			</div>
-
-			<div class="email-footer">
-				Sent from <?php echo esc_html( get_bloginfo( 'name', 'display' ) ); ?>
-			</div>
-		</div>
+	<body marginwidth="0" topmargin="0" marginheight="0" offset="0">
+		<table width="100%" id="outer_wrapper">
+			<tr>
+				<td><!-- Deliberately empty to support consistent sizing and layout across multiple email clients. --></td>
+				<td width="600">
+					<div id="email_wrapper">
+						<table border="0" cellpadding="0" cellspacing="0" height="100%" width="100%" id="inner_wrapper">
+							<tr>
+								<td align="center" valign="top">
+									<table border="0" cellpadding="0" cellspacing="0" width="100%">
+										<tr>
+											<td id="template_header_image">
+												<?php
+												if ( $header_logo ) {
+													echo '<p style="margin-top:0;"><img src="' . esc_url( $header_logo ) . '" alt="' . esc_attr( $site_name ) . '" /></p>';
+												} else {
+													echo '<p class="email-logo-text">' . esc_html( $site_name ) . '</p>';
+												}
+												?>
+											</td>
+										</tr>
+									</table>
+									<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_container">
+										<tr>
+											<td align="center" valign="top">
+												<!-- Header -->
+												<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_header">
+													<tr>
+														<td id="header_wrapper">
+															<h1><?php echo esc_html( $email_subject ); ?></h1>
+														</td>
+													</tr>
+												</table>
+												<!-- End Header -->
+											</td>
+										</tr>
+										<tr>
+											<td align="center" valign="top">
+												<!-- Body -->
+												<table border="0" cellpadding="0" cellspacing="0" width="100%" id="template_body">
+													<tr>
+														<td valign="top" id="body_content">
+															<!-- Content -->
+															<table border="0" cellpadding="20" cellspacing="0" width="100%">
+																<tr>
+																	<td valign="top" id="body_content_inner_cell">
+																		<div id="body_content_inner">
+																			<?php echo wp_kses_post( wpautop( $email_content ) ); ?>
+																		</div>
+																	</td>
+																</tr>
+															</table>
+															<!-- End Content -->
+														</td>
+													</tr>
+												</table>
+												<!-- End Body -->
+											</td>
+										</tr>
+									</table>
+								</td>
+							</tr>
+							<tr>
+								<td align="center" valign="top">
+									<!-- Footer -->
+									<table border="0" cellpadding="10" cellspacing="0" width="100%" id="template_footer">
+										<tr>
+											<td valign="top">
+												<table border="0" cellpadding="10" cellspacing="0" width="100%">
+													<tr>
+														<td colspan="2" valign="middle" id="credit">
+															<p>Sent from <?php echo esc_html( get_bloginfo( 'name', 'display' ) ); ?></p>
+														</td>
+													</tr>
+												</table>
+											</td>
+										</tr>
+									</table>
+									<!-- End Footer -->
+								</td>
+							</tr>
+						</table>
+					</div>
+				</td>
+				<td><!-- Deliberately empty to support consistent sizing and layout across multiple email clients. --></td>
+			</tr>
+		</table>
 	</body>
 </html>
